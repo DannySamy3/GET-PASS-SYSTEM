f2cf654ee4e58b88a70c4d65f9bb1caaf72895b4
DannySamy3 <danielntunduye@gmail.com>
2025-03-09 08:49:25 +0300

    fixed editing image errors

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,8 @@ app.use(cors());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(bodyParser.json({ limit: "80mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 app.use("/getPass/students", studentRoute);
 app.use("/getPass/classes", classRoutes);
 app.use("/getPass/sponsors", sponsorRoute);
