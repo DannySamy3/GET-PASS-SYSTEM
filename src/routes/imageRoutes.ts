9926f5b45e1a9dbc7badeb5cfcb73f964ddc4e3e
DannySamy3 <danielntunduye@gmail.com>
2025-03-24 14:20:54 +0300

    new fixes

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { editImageController } from "../controllers/imageController";
+import {
+  editImageController,
+  handleImageUpload,
+} from "../controllers/imageController";
 import { upload } from "../gcpStorage";
 
 const router = Router();
 
-router.post("/edit", upload.single("photo"), editImageController);
+// router.post("/edit", upload.single("photo"), editImageController);
+router.post("/edit", handleImageUpload, editImageController);
 
 export default router;
